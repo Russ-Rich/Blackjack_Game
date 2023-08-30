13f5de7e1b518511b18849a32ea88ceeca72899b
Russ <russ3750@gmail.com>
2023-08-29 19:39:08 -0500

    Building game foundation

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstCard = 10;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+if (sum <= 20) {
+  
+  console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+  console.log("Woohoo!!! You got BlackJack!");
+  hasBlackJack = true;
+} else {
+  console.log("You are out of the game!");
+  isAlive = false
+}
+
+// CASH OUT!
+console.log(isAlive)
