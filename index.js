bb420d2a7282305a3505fdfab420223faf1cee4d
Russ <russ3750@gmail.com>
2023-09-06 18:11:04 -0500

    update index.html and index.js

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ let hasBlackJack = false;
 let isAlive = true;
 
 if (sum <= 20) {
-  
-  console.log("Do you want to draw a new card?");
+	console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
-  console.log("Woohoo!!! You got BlackJack!");
-  hasBlackJack = true;
+	console.log("Woohoo!!! You got BlackJack!");
+	hasBlackJack = true;
 } else {
-  console.log("You are out of the game!");
-  isAlive = false
+	console.log("You are out of the game!");
+	isAlive = false;
 }
 
 // CASH OUT!
-console.log(isAlive)
+console.log(isAlive);
